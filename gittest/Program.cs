584d711b8b4c8e0f84dea9bcb8b76ed3d4e32e6f
ArtemS <you@example.com>
2021-03-10 16:38:39 +0100

    added gitignore

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -6,7 +6,7 @@ namespace gittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldzzzzz!");
+            Console.WriteLine("Hello Worldzzzzzgit !");
         }
     }
 }
