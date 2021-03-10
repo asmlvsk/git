a250064562ab47b293d50af97e75ec2070804920
ArtemS <smoilovskyi151@gmail.com>
2021-03-10 18:02:06 +0100

    changed

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -6,7 +6,7 @@ namespace gittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldzzzzzgit !");
+            Console.WriteLine("Hello Worldzzzzzgitвввввв !");
         }
     }
 }
